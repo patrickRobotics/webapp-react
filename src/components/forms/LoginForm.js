5d362626655f0645df49a4a7cddcd7c22c683167
patrickRobotics <pirungu9@gmail.com>
2020-01-14 19:22:45 +0300

    Login form setup

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Form, Button } from "semantic-ui-react";
+
+class LoginForm extends React.Component {
+
+    state = {
+        data: {},
+        loading: false,
+        errors: {}
+    }
+
+
+    render() {
+        return(
+            <Form>
+                <Button primary>Login</Button>
+            </Form>
+        );
+    }
+}
+
+
+export default LoginForm;
