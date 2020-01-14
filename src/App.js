e2ae74159057999590a4ea425026b356f4dc9e34
patrickRobotics <pirungu9@gmail.com>
2020-01-14 19:12:12 +0300

    React Router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
+import {Route} from "react-router";
 
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
     </div>
-  );
-}
+);
 
 export default App;
