d6f9c0a540a05fb99071dcc1706da3887a0919a9
John Doe <pirungu9@gmail.com>
2021-01-12 13:17:17 +0300

    remove jest related function

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,4 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Learn React/i);
-  expect(linkElement).toBeInTheDocument();
 });
